db8d066faa7ba6e86516832cdc22defc2d9eaa45
Aditya Kumar <aditya1806kumar@gmail.com>
2026-07-09 22:46:29 +0530

    structured way

---
+++ utils/Evaluation_plot.py
@@ -0,0 +1,40 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.metrics import classification_report, confusion_matrix
+
+def featureimportance(important_col,importances):
+    feature_imp_df = pd.DataFrame({
+        'Feature': important_col,
+        'Importance': importances
+    }).sort_values(by='Importance', ascending=False)
+
+    plt.figure(figsize=(8, 4))
+    sns.barplot(data=feature_imp_df, x='Importance', y='Feature', palette='viridis')
+    plt.title('What Drives Ticket Priority? ')
+    plt.xlabel('Importance Score')
+    plt.ylabel('Features')
+    plt.tight_layout()
+    plt.show()
+
+def confusionmatrix(y_test,y_pred):
+    print("Classification report")
+    print(classification_report(y_test, y_pred))
+    
+    plt.figure(figsize=(6, 4))
+    sns.heatmap(confusion_matrix(y_test, y_pred), annot=True, fmt='d', cmap='Blues',
+                xticklabels=['P1', 'P2', 'P3'], yticklabels=['P1', 'P2', 'P3'])
+    plt.title('Confusion Matrix (Actual vs Predicted Categories)')
+    plt.ylabel('Actual Priority')
+    plt.xlabel('Predicted Priority')
+    plt.show()
+
+    
+def correlation(df,olumn):
+    plt.figure(figsize=(12, 8))
+    corr_matrix = df[column].corr()
+    sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', fmt=".2f")
+    plt.title('Correlation Matrix')
+    plt.tight_layout()
+    plt.show()
